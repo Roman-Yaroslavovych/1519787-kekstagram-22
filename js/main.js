35bdb03a3456ebf1ff8084c05199be1b97113708
Roman-Yaroslavovych <romanmarysun@gmail.com>
2021-02-02 06:44:11 +0100

    change

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 // Функция для проверки максимальной длины строки введённого комментария.
 
-let calculateCommentLenght = function ( isResevedCommentLenght, maxCommentLenght ) {
+const calculateCommentLenght = function ( isResevedCommentLenght, maxCommentLenght ) {
   if (isResevedCommentLenght <= maxCommentLenght) {return true}
   return false
 };
 
-calculateCommentLenght ();
 
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
 
-let getRandomIntInclusive = function (min, max) {
-  min = Math.ceil(min);
+const  getRandomInteger = function (min, max) {
+  min = Math.round(min);
   max = Math.floor(max);
-  if (min >= 0 && max >= min) { return Math.floor(Math.random() * (max - min + 1)) + min}
-  return false
+  if (min >= 0 && max >= min) {
+    return Math.floor(Math.random() * (max - min + 1)) + min}
 }
 
-getRandomIntInclusive ();
